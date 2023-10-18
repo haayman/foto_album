60961c5d719d015fd8293a9e0b94f3954fe48a22
Arjen Haayman <arjen.haayman@gmail.com>
2023-10-18 17:23:58 +0200

    retry 5x

--- parseAlbum.js
+++ parseAlbum.js
@@ -1,10 +1,23 @@
-onmessage = async ({data:link}) => {
-
-    const response = await fetch(`/wp-admin/admin-ajax.php?action=plusleo_album&link=${encodeURIComponent(link)}`).then(res => res.text());
-    const regex = new RegExp('"(?<url>http[^"]+)",[0-9^,]+,[0-9^,]+', 'gi');
-    const urls = [];
-    for( const match of response.matchAll(regex) ) {
+onmessage = async ({ data: link }) => {
+  let retries = 5;
+  while (retries--) {
+    try {
+      const response = await fetch(
+        `/wp-admin/admin-ajax.php?action=foto_album&link=${encodeURIComponent(
+          link
+        )}`
+      ).then((res) => res.text());
+      const regex = new RegExp('"(?<url>http[^"]+)",[0-9^,]+,[0-9^,]+', "gi");
+      const urls = [];
+      for (const match of response.matchAll(regex)) {
         urls.push(match.groups.url);
+      }
+      if(!urls.length) throw new Error("No urls found");
+      postMessage(urls);
+      return;
+    } catch (e) {
+      console.log(`retrying ${link}...(${retries} retries left)`);
+      await new Promise((r) => setTimeout(r, 500));
     }
-    postMessage(urls);
-}
+  }
+};
